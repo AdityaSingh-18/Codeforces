b8c42e627d850deefa17a17ddf81baed167ba8b7
AdityaSingh <anshul8303858511@gmail.com>
2025-12-24 12:51:07 +0530

    Add Blackslex and Password solution

---
+++ Unrated/A_Blackslex_and_Password.cpp
@@ -0,0 +1,19 @@
+// Problem Link https://codeforces.com/contest/2179/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    int n, m;
+    cin >> n >> m;
+    cout << n * m + 1 << "\n";
+}
+
+signed main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
