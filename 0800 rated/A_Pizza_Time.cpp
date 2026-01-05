5428bfab4fd28015eb6a9036bcf66bbbac51054d
AdityaSingh <anshul8303858511@gmail.com>
2026-01-05 22:56:44 +0530

    Add Pizza Time solution

---
+++ 0800 rated/A_Pizza_Time.cpp
@@ -0,0 +1,19 @@
+// Problem Link https://codeforces.com/contest/2156/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve(){
+    int n;
+    cin >> n;
+    cout << (n - 1) / 2 << "\n";
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
