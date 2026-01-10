b3efd71c428865031640117f1bacf47eb71ad9a3
AdityaSingh <anshul8303858511@gmail.com>
2026-01-10 22:06:56 +0530

    Add Good Start solution

---
+++ 1200 rated/B_Good_Start.cpp
@@ -0,0 +1,32 @@
+// Problem Link https://codeforces.com/contest/2113/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve(){
+    int w, h, a, b;
+    cin >> w >> h >> a >> b;
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    if(x1 == x2){
+        if(abs(y1 - y2) % b == 0)    cout << "Yes\n";
+        else    cout << "No\n";
+        return;
+    }
+    if(y1 == y2){
+        if(abs(x1 - x2) % a == 0)   cout << "Yes\n";
+        else    cout << "No\n";
+        return;
+    }
+    if((x1 - x2) % a == 0 || (y1 - y2) % b == 0)    cout << "Yes\n";
+    else    cout << "No\n";
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
