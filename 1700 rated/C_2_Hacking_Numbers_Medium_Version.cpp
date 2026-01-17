5ccecb4b722b23bd2f4640e70a97b4f172352bbd
AdityaSingh <anshul8303858511@gmail.com>
2026-01-17 19:25:20 +0530

    Add Hacking Numbers Medium Version solution

---
+++ 1700 rated/C_2_Hacking_Numbers_Medium_Version.cpp
@@ -0,0 +1,29 @@
+// Problem Link https://codeforces.com/contest/2109/problem/C2
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    int n;
+    cin >> n;
+    cout << "mul " << 9 << "\n";
+    int x;
+    cin >> x;
+    cout << "digit" << "\n";
+    cin >> x;
+    cout << "digit" << "\n";
+    cin >> x;
+    cout << "add " << n - 9 << "\n";
+    cin >> x;
+    cout << "!" << "\n";
+    cin >> x;
+}
+
+signed main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
