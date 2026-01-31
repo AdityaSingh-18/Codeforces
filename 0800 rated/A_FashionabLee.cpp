afa7c1ae294574d1cf3f65e30aa3232bd58e3699
AdityaSingh <anshul8303858511@gmail.com>
2026-01-31 14:41:18 +0530

    Add FashionabLee solution

---
+++ 0800 rated/A_FashionabLee.cpp
@@ -0,0 +1,20 @@
+// Problem Link https://codeforces.com/contest/1369/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    int n;
+    cin >> n;
+    if(n % 4)   cout << "NO\n";
+    else    cout << "YES\n";
+}
+
+signed main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
