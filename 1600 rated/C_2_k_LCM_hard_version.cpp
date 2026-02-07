03dad9bad4090cbc62a769438752ee3e9f2d20ad
AdityaSingh <anshul8303858511@gmail.com>
2026-02-07 16:53:34 +0530

    Add k LCM hard version solution

---
+++ 1600 rated/C_2_k_LCM_hard_version.cpp
@@ -0,0 +1,33 @@
+// Problem Link https://codeforces.com/contest/1497/problem/C2
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    int x = k - 3;
+    n -= (k - 3);
+    while(x > 0){
+        cout << 1 << " ";
+        x--;
+    }
+    if(n % 2){
+        cout << n / 2 << " " << n / 2 << " " << 1 << "\n";
+    }
+    else if(n % 4){
+        cout << n / 2 - 1 << " " << n / 2 - 1 << " " << 2 << "\n";
+    }
+    else{
+        cout << n / 2 << " " << n / 4 << " " << n / 4 << "\n";
+    }
+}
+
+signed main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
