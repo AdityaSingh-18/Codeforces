c0b722c41b54896da5ce4fe7ed33abaeab1856b0
AdityaSingh <anshul8303858511@gmail.com>
2026-02-16 16:28:21 +0530

    Add Dice Roll Sequence solution

--- Unrated/C_Dice_Roll_Sequence.cpp
+++ Unrated/C_Dice_Roll_Sequence.cpp
@@ -9,28 +9,14 @@ void solve(){
     cin >> n;
     vector<int> a(n);
     for (auto &i : a)    cin >> i;
-    vector<vector<int>> dp(n, vector<int>(7, 1e18));
-    for (int j = 1; j <= 6; j++) {
-        if(a[0] == j)   dp[0][j] = 0;
-        else    dp[0][j] = 1;
-    }    
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j <= 6; j++) {
-            int cost = 1;
-            if(a[i] == j)     cost = 0;
-            for (int k = 1; k <= 6; k++) {
-                if(j == k || (j + k) == 7)      continue;
-                if (dp[i - 1][k] != 1e18) {
-                    dp[i][j] = min(dp[i][j], dp[i - 1][k] + cost);
-                }
-            }
+    int count = 0;
+    for (int i = 1; i < n; i++){
+        if(a[i] == a[i - 1] || a[i] + a[i - 1] == 7){
+            count++;
+            i++;
         }
-    }    
-    int ans = 1e18;
-    for (int j = 1; j <= 6; j++) {
-        ans = min(ans, dp[n - 1][j]);
     }
-    cout << ans << "\n";
+    cout << count << "\n";
 }
 
 signed main(){
