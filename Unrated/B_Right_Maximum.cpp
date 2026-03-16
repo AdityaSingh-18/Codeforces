63b0999e5188edb03a1ead544ebd3181668a75dc
AdityaSingh <anshul8303858511@gmail.com>
2026-03-16 22:47:13 +0530

    Add Right Maximum solution

---
+++ Unrated/B_Right_Maximum.cpp
@@ -0,0 +1,28 @@
+// Problem Link https://codeforces.com/contest/2204/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &i : a)    cin >> i;
+    int currMax = 0, count = 0;
+    for (int i = 0; i < n; i++){
+        if(a[i] >= currMax){
+            count++;
+            currMax = a[i];
+        }
+    }
+    cout << count << "\n";    
+}
+
+signed main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
