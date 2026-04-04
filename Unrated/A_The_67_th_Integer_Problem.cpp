e7bff8b7c8961e8fb243590b436207b97484e116
AdityaSingh <anshul8303858511@gmail.com>
2026-04-04 22:23:46 +0530

    Add The 67th Integer Problem

---
+++ Unrated/A_The_67_th_Integer_Problem.cpp
@@ -0,0 +1,20 @@
+// Problem Link https://codeforces.com/contest/2218/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    int n;
+    cin >> n;
+    if(n == 67)   cout << n << "\n";
+    else    cout << n + 1 << "\n";
+}
+
+signed main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
